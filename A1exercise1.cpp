e6024d5ec4771047f8dde957744a1d714c4edf46
api <e2202869@edu.vamk.fi>
2023-09-07 11:12:54 +0300

    new file

--- A1exercise1.cpp
+++ A1exercise1.cpp
@@ -8,37 +8,40 @@ void countSecondSeries(void);
 // main function
 int main(void)
 {
-    cout << "First 15 numbers of the first series:" << endl;  // printing header text
-    countFirstSeries();                                       // calling the first function which counts and prints the first series of numbers
-    cout << endl;                                             // new line
+    cout << "First 15 numbers of the first series:" << endl; // printing header text
+    countFirstSeries();                                      // calling the first function which counts and prints the first series of numbers
+    cout << endl;
     cout << "First 15 numbers of the second series:" << endl; // printing header text
     countSecondSeries();                                      // calling the second function which counts and prints the second series of numbers
 }
 
 // function definitions
+
+// counting the first series of numbers and printing it out
 void countFirstSeries(void)
 {
-    // variables which are used inside the for loop
     int i;
     int j = 0;
 
     for (i = 1; i <= 7; i++) // looping 7 times, because we want to print 15 numbers (7x2 prints + 1 print after the loop = 15 prints)
     {
-        cout << i << "," << j << ","; // printing i and j variables
-        j++;                          // incrementing j for the next loop round
+        cout << i << "," << j << ",";
+        j++;
     }
 
     cout << i << "," << endl; // printing once after the loop for the 15th number of the series
 }
 
+// counting the second series of numbers and printing it out
+// the next number will always be the sum of the last three numbers
+// for an example number after numbers 111 will be number 3 (1+1+1)
 void countSecondSeries(void)
 {
-    // variables which are used inside the loop
     int i;
-    int x = 1, y = 1, z = 1; // xyz variables are 1's for the start
-    for (i = 0; i < 5; i++)  // looping 5 times, because we want to print 15 numbers (5x3 prints = 15 prints)
+    int x = 1, y = 1, z = 1;
+    for (i = 0; i < 5; i++) // looping 5 times, because we want to print 15 numbers (5x3 prints = 15 prints)
     {
-        cout << x << "," << y << "," << z << ","; // printing xyz
+        cout << x << "," << y << "," << z << ",";
         // adding variables together for the next loop
         x += y + z;
         y += x + z;
---
+++ A1exercise2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+// declaring functions
+int askInput(void);
+int countSumBetween(int num1, int num2);
+
+// main function
+int main(void)
+{
+    // declaring variables
+    int temp;
+    int sum;
+    // number1 and 2 will get their values from askInput() function
+    int number1 = askInput();
+    int number2 = askInput();
+
+    // making sure that number1 is smaller than number2
+    if (number1 > number2)
+    {
+        // if the number1 is bigger than number2, then their values are swapped
+        temp = number1;
+        number1 = number2;
+        number2 = temp;
+    }
+
+    sum = countSumBetween(number1, number2); // sum variable gets its value from countSumBetween() function
+    cout << "Sum of numbers between the given numbers " << number1 << " and " << number2 << " is: " << endl
+         << sum; // printing out the sum
+    return 0;
+}
+
+// defining functions
+
+// asking for a number from user and returning it to the main() function
+int askInput(void)
+{
+    int userInput;
+    cout << "Give the first number: ";
+    cin >> userInput;
+    return userInput;
+}
+
+// counting sum of the numbers between the given numbers, and returning it to main() function
+// for an example if the given numbers are 2 and 6, then it counts the sum of number 3, 4 and 5
+int countSumBetween(int num1, int num2)
+{
+    int i = num1 + 1;
+    int sum;
+
+    for (i; i < num2; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
