aec66a444600f4e3f3d99e6d5447ccd27b463f65
api <e2202869@edu.vamk.fi>
2023-09-29 11:49:34 +0300

    new exercise

---
+++ A32.cpp
@@ -0,0 +1,50 @@
+/*A program which reads a string from the standard input and prints
+the content of the string in ascending order. Use pointer(s) in the string handling.*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+// variables
+string user_input;
+
+// declaring functions
+void sortString();
+
+int main(void)
+{
+    // asking for input
+    cout << "Give input: ";
+    getline(cin, user_input);
+    // calling function to sort and print the input
+    sortString();
+}
+
+// defining functions
+
+// function which sorts the content of the string in ascending order
+void sortString(void)
+{
+    int LENGTH = user_input.length(); // variable which holds the length of our string
+
+    // bubble sort algorithm
+    for (int i = 0; i < LENGTH; i++)
+    {
+        for (int j = 0; j < LENGTH - 1; j++)
+        {
+            // declaring pointers
+            char *p1 = &user_input[j];
+            char *p2 = p1 + 1;
+            // comparing pointers
+            if (*p1 > *p2)
+            {
+                char temp = *p1;
+                *p1 = *p2;
+                *p2 = temp;
+            }
+        }
+    }
+
+    // printing out the sorted string
+    cout << "content of the string in ascending order: " << endl
+         << user_input;
+}
