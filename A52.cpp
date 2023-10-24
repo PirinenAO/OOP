59225e4626c73764bd2e3eadfe9a58d0c12259af
api <e2202869@edu.vamk.fi>
2023-10-24 15:16:34 +0300

    exercise

--- A52.cpp
+++ A52.cpp
@@ -1,55 +1,48 @@
+/*A program in which you use recursion to calculate the following sums:
+1⁄2 + 1⁄4 + 1⁄6+...+1⁄2n
+-1⁄3 * -2⁄5 * -3⁄7*...*-n⁄(2n+1)*/
+
 #include <iostream>
 using namespace std;
 
-double firstSum(double);
-double secondSum(double);
+double sumFunction(double);
+double multiplicationFunction(double);
 
 int main(void)
 {
-    double x = 3;
-    double y = -3;
+    double n = 3;
 
     // first sequence
-    cout << "sum of numbers: " << endl;
-    double sum1 = firstSum(x);
-    cout << " is: ";
+    cout << "first: " << endl;
+    double sum1 = sumFunction(n);
     std::cout << sum1 << endl;
 
     // second sequence
     cout << endl
-         << "multiplication of numbers: " << endl;
-    double sum2 = secondSum(y);
-    cout << " is: ";
+         << "second:  " << endl;
+    double sum2 = multiplicationFunction(n);
     std::cout << sum2;
 
     return 0;
 }
 
-double firstSum(double n)
+double sumFunction(double n)
 {
-    while (n)
+    if (!n)
     {
-        cout << "1/" << 2 * n;
-        // if statement for adding plus signs
-        if (n != 1) // not adding plus sign after the last number
-        {
-            cout << " + ";
-        }
-
-        // adding the sums together in descending order
-        return 1.0 / (2 * n) + firstSum(n - 1);
+        return 0;
     }
 
-    return 0;
+    return 1.0 / (2 * n) + sumFunction(n - 1);
 }
 
-double secondSum(double n)
+double multiplicationFunction(double n)
 {
-    while (n != 0)
+
+    if (!n)
     {
-        cout << n << "/" << (2 * n) + 1 << " * ";
-        return (n / (2 * n)) * secondSum(n + 1);
+        return 1;
     }
 
-    return 0;
+    return -n / (2 * n + 1) * multiplicationFunction(n - 1);
 }
